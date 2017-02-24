d477a1f73e9794fbad23b5d53df9c531fa951b44
André Luiz Hashiguchi <andre.hashiguchi@onclicksistemas.com.br>
2017-02-24 14:48:23 -0300

    Evalutating refactoring

--- Clojure/EvaluatingEX.clj
+++ Clojure/EvaluatingEX.clj
@@ -1,14 +1,11 @@
 (use '[clojure.string :only (split triml)])
 
-(defn fact[x]
-  (if (<= x 1)
-    1
-    (* x (fact (- x 1))
-  )))
+(defn factorial [n]
+    )
 
 (let [n_t (read-line) n (Integer/parseInt n_t) ]
   (loop [a0 n]
     (when (> a0 0)
       (let [x_t (read-line) x (Float/parseFloat x_t) ]            
-        (println(reduce + (for [i (range 10)] (/ (Math/pow x i) (fact i))))))
+        (println(reduce + (for [i (range 10)] (/ (Math/pow x i) (reduce * (range 1 (inc i))))))))
     (recur (- a0 1)))))
