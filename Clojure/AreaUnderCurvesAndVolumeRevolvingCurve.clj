a49ebf35014ef416c86621b8149da96e2102d801
André Luiz Hashiguchi <andre.hashiguchi@onclicksistemas.com.br>
2017-05-18 22:41:54 -0300

    Alterações

---
+++ Clojure/AreaUnderCurvesAndVolumeRevolvingCurve.clj
@@ -0,0 +1,12 @@
+; Enter your code here. Read input from STDIN. Print output to STDOUT
+;
+(let
+    [
+        F (map #(Integer. %) (clojure.string/split (read-line) #"\s+"))
+        P (map #(Integer. %) (clojure.string/split (read-line) #"\s+"))
+        [lo hi] (map #(Integer. %) (clojure.string/split (read-line) #"\s+"))
+        f (fn [x] (->> P (map #(Math/pow x %)) (map * F) (reduce +)))
+        r (->> (range lo (+ 0.001M hi) 0.001M) (map f))
+   ]
+  (printf "%.1f\n" (* (->> r (reduce +)) 0.001M))
+  (printf "%.1f\n" (->> r (map #(* (* Math/PI % %) 0.001M)) (reduce +))))
---
+++ Python/printfunction.py
@@ -0,0 +1,9 @@
+from __future__ import print_function
+if __name__ == '__main__':
+    n = int(raw_input())
+
+    number = ""
+    for i in range(n):
+        number += str(i + 1)
+
+    print (number)
